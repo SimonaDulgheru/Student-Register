8ac061c4d9df5709370ede8309f661a4d56b245b
Simona <s.c.dulgheru@gmail.com>
2020-05-23 14:44:52 +0100

    Teacher, Present Student and Absent Student classes added to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+class RegisterTeacher {
+	constructor(teacher, subject) {
+		this.teacher = teacher;
+		this.subject = subject;
+	}
+}
+
+class StudentPresent {
+	constructor(name) {
+		this.name = name;
+	}
+}
+
+class StudentAbsent {
+	constructor(name) {
+		this.name = name;
+	}
+}
